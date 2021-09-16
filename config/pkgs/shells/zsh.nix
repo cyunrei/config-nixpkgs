a16fdfe94d2dd8e3cc808647a611ed6634796f40
Cyunrei <cyunrei@gmail.com>
2021-09-16 23:19:07 +0800

    Structured config to matching nixpkgs

---
+++ config/pkgs/shells/zsh.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+
+{
+  programs = {
+    zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      initExtra = ''
+        source ${./../../../res/p10k.zsh}
+        source $HOME/.nix-profile/etc/profile.d/nix.sh
+        # https://github.com/romkatv/powerlevel10k#manual
+        source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+        # https://github.com/zdharma/fast-syntax-highlighting#manual
+        source ${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh
+      '';
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ "git" ];
+      };
+      shellAliases = {
+        top = "${pkgs.htop}/bin/htop";
+        cls = "clear";
+        hm = "home-manager";
+      };
+    };
+  };
+}
