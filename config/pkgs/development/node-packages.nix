c2997d6e1d2827ce18d6ec7f68dab295c04b6123
Cyunrei <cyunrei@gmail.com>
2021-09-19 17:15:09 +0800

    Fix cannot install package globally with npm and add env path

--- config/pkgs/development/node-packages.nix
+++ config/pkgs/development/node-packages.nix
@@ -5,11 +5,17 @@
   home = {
     file = {
       ".npmrc".text = ''
-      registry=https://registry.npm.taobao.org
+        registry=https://registry.npm.taobao.org
+
+        # Fix cannot install package globally with npm
+        # See https://www.reddit.com/r/NixOS/comments/8cnm50/cannot_install_package_globally_with_npm/
+        prefix=~/.node_modules
       '';
     };
     packages = with pkgs; [
       nodePackages.npm
     ];
+    sessionPath = [ "$HOME/.node_modules/bin/" ];
   };
+
 }
