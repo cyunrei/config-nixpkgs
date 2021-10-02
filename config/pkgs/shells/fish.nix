bdf7c48a2436d5be9e29750a9eb798b08f5ed82e
Cyunrei <cyunrei@gmail.com>
2021-10-02 10:32:28 +0800

    Add docker.fish

--- config/pkgs/shells/fish.nix
+++ config/pkgs/shells/fish.nix
@@ -54,6 +54,14 @@
         sha256 = "sha256-3HHcaX2yVZXrRjOeSlI8tPhSjRR2tPU6AWq6eU5nPOs=";
       };
     }
+    {
+      name = "docker.fish";
+      src = pkgs.fetchgit {
+        url = "https://github.com/Cyunrei/docker.fish";
+        rev = "7dce1b32973833ffa7024b740869e826357b3082";
+        sha256 = "090cb77kwxw867rrp6f3rh623vflh22l24r83dgxav55kakncz90";
+      };
+    }
   ];
     };
     starship = {
