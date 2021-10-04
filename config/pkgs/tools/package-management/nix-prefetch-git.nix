5939e4c2db3cc4e447e5f1275e9b7d1365d6e294
Cyunrei <cyunrei@gmail.com>
2021-10-05 00:24:01 +0800

    Optimize package import

---
+++ config/pkgs/tools/package-management/nix-prefetch-git.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+
+{
+  home = {
+    packages = with pkgs; [
+      nix-prefetch-git
+    ];
+  };
+}
--- home.nix
+++ home.nix
@@ -5,27 +5,35 @@
     username = "cyunrei";
     homeDirectory = "/home/cyunrei";
     stateVersion = "21.11";
-    packages = with pkgs; [
-      conda
-      nixpkgs-fmt
-      nodejs
-      nix-prefetch-git
-    ];
   };
 
   imports = [
     config/pkgs/applications/editors/neovim.nix
+
     config/pkgs/applications/misc/hugo.nix
     config/pkgs/applications/misc/ranger.nix
+
     config/pkgs/applications/version-management/git.nix
+
     config/pkgs/development/tools/build-managers/gradle.nix
     config/pkgs/development/tools/yarn.nix
+    
+    config/pkgs/development/web/nodejs.nix
+
     config/pkgs/development/node-packages.nix
+
     config/pkgs/shells/fish.nix
     config/pkgs/shells/zsh.nix
+
     config/pkgs/tools/misc/bat.nix
     config/pkgs/tools/misc/exa.nix
+
+    config/pkgs/tools/nix/nixpkgs-fmt.nix
+
+    config/pkgs/tools/package-management/conda.nix
     config/pkgs/tools/package-management/home-manager.nix
+    config/pkgs/tools/package-management/nix-prefetch-git.nix
+
     config/pkgs/tools/system/htop.nix
   ];
 }
