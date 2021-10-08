629fb4e2874cd7239da1db0dc328bceface7f7f0
Cyunrei <cyunrei@gmail.com>
2021-10-08 17:44:33 +0800

    Format code

--- config/pkgs/shells/fish.nix
+++ config/pkgs/shells/fish.nix
@@ -4,7 +4,7 @@
     fish = {
       enable = true;
       shellInit = ''
-         replay source $HOME/.nix-profile/etc/profile.d/nix.sh
+        replay source $HOME/.nix-profile/etc/profile.d/nix.sh
       '';
       shellAliases = {
         top = "${pkgs.htop}/bin/htop";
@@ -15,55 +15,55 @@
         unsetproxy = "set --erase http_proxy && set --erase https_proxy";
       };
       plugins = [
-    {
-      name = "done";
-      src = pkgs.fetchgit {
-        url = "https://github.com/franciscolourenco/done";
-        rev = "1.16.3";
-        sha256 = "sha256-Xld66z9vVp3kxikndZ9k/zlNvP0YSoSCNTBkJ8rT3uo=";
-      };
-    }
-    {
-      name = "pisces";
-      src = pkgs.fetchgit {
-        url = "https://github.com/laughedelic/pisces";
-        rev = "v0.7.0";
-        sha256 = "sha256-Oou2IeNNAqR00ZT3bss/DbhrJjGeMsn9dBBYhgdafBw=";
-      };
-    }
-    {
-      name = "plugin-bang-bang";
-      src = pkgs.fetchgit {
-        url = "https://github.com/oh-my-fish/plugin-bang-bang";
-        rev = "f969c618301163273d0a03d002614d9a81952c1e";
-        sha256 = "sha256-A8ydBX4LORk+nutjHurqNNWFmW6LIiBPQcxS3x4nbeQ=";
-      };
-    }
-    {
-      name = "replay.fish";
-      src = pkgs.fetchgit {
-        url = "https://github.com/jorgebucaran/replay.fish";
-        rev = "1.2.0";
-        sha256 = "sha256-Q/9YVdiRSJw1SdcfQv2h7Lj6EyFustRk+kmh1eRRQ6k=";
-      };
-    }
-    {
-      name = "nix-completions.fish";
-      src = pkgs.fetchgit {
-        url = "https://github.com/kidonng/nix-completions.fish";
-        rev = "dcb15c4f7d3e85d8f7cd3eb09b3014ccd278aab8";
-        sha256 = "sha256-3HHcaX2yVZXrRjOeSlI8tPhSjRR2tPU6AWq6eU5nPOs=";
-      };
-    }
-    {
-      name = "docker.fish";
-      src = pkgs.fetchgit {
-        url = "https://github.com/Cyunrei/docker.fish";
-        rev = "7dce1b32973833ffa7024b740869e826357b3082";
-        sha256 = "090cb77kwxw867rrp6f3rh623vflh22l24r83dgxav55kakncz90";
-      };
-    }
-  ];
+        {
+          name = "done";
+          src = pkgs.fetchgit {
+            url = "https://github.com/franciscolourenco/done";
+            rev = "1.16.3";
+            sha256 = "sha256-Xld66z9vVp3kxikndZ9k/zlNvP0YSoSCNTBkJ8rT3uo=";
+          };
+        }
+        {
+          name = "pisces";
+          src = pkgs.fetchgit {
+            url = "https://github.com/laughedelic/pisces";
+            rev = "v0.7.0";
+            sha256 = "sha256-Oou2IeNNAqR00ZT3bss/DbhrJjGeMsn9dBBYhgdafBw=";
+          };
+        }
+        {
+          name = "plugin-bang-bang";
+          src = pkgs.fetchgit {
+            url = "https://github.com/oh-my-fish/plugin-bang-bang";
+            rev = "f969c618301163273d0a03d002614d9a81952c1e";
+            sha256 = "sha256-A8ydBX4LORk+nutjHurqNNWFmW6LIiBPQcxS3x4nbeQ=";
+          };
+        }
+        {
+          name = "replay.fish";
+          src = pkgs.fetchgit {
+            url = "https://github.com/jorgebucaran/replay.fish";
+            rev = "1.2.0";
+            sha256 = "sha256-Q/9YVdiRSJw1SdcfQv2h7Lj6EyFustRk+kmh1eRRQ6k=";
+          };
+        }
+        {
+          name = "nix-completions.fish";
+          src = pkgs.fetchgit {
+            url = "https://github.com/kidonng/nix-completions.fish";
+            rev = "dcb15c4f7d3e85d8f7cd3eb09b3014ccd278aab8";
+            sha256 = "sha256-3HHcaX2yVZXrRjOeSlI8tPhSjRR2tPU6AWq6eU5nPOs=";
+          };
+        }
+        {
+          name = "docker.fish";
+          src = pkgs.fetchgit {
+            url = "https://github.com/Cyunrei/docker.fish";
+            rev = "7dce1b32973833ffa7024b740869e826357b3082";
+            sha256 = "090cb77kwxw867rrp6f3rh623vflh22l24r83dgxav55kakncz90";
+          };
+        }
+      ];
     };
     starship = {
       enable = true;
