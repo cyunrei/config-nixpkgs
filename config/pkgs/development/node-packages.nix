56b56210c00b5ccfeed28bd51c24bc45adce134f
Cyunrei <cyunrei@gmail.com>
2022-01-20 03:51:38 +0800

    Change the symlinks from /nix/store back to source (res) file

--- config/pkgs/development/node-packages.nix
+++ config/pkgs/development/node-packages.nix
@@ -4,13 +4,7 @@
 {
   home = {
     file = {
-      ".npmrc".text = ''
-        registry=https://registry.npm.taobao.org
-
-        # Fix cannot install package globally with npm
-        # See https://www.reddit.com/r/NixOS/comments/8cnm50/cannot_install_package_globally_with_npm/
-        prefix=~/.node_modules
-      '';
+      ".npmrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/res/.npmrc";
     };
     packages = with pkgs; [
       nodePackages.npm
--- config/pkgs/development/tools/yarn.nix
+++ config/pkgs/development/tools/yarn.nix
@@ -4,14 +4,14 @@
 {
   home = {
     file = {
-      ".yarnrc".text = ''
-        registry "https://registry.npm.taobao.org/"
-      '';
+      # Fix Error: EACCES: permission denied
+      # https://github.com/nix-community/home-manager/issues/676#issuecomment-787901002
+      ".yarnrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/res/.yarnrc";
     };
     packages = with pkgs; [
       pkgs.yarn
     ];
     sessionPath = [ "$HOME/.yarn/bin/" ];
   };
-  
+
 }
