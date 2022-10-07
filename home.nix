790c851f315cf9dd7ce1118072beb3b4a56e389d
Cyunrei <cyunrei@gmail.com>
2022-10-08 05:47:43 +0800

    Remove some pkgs

--- home.nix
+++ home.nix
@@ -9,14 +9,8 @@
 
   imports = [
 
-    config/pkgs/applications/misc/hugo.nix
-    config/pkgs/applications/misc/ranger.nix
-
-    config/pkgs/applications/system/glances.nix
-
     config/pkgs/applications/version-management/git.nix
 
-    config/pkgs/development/tools/build-managers/gradle.nix
     config/pkgs/development/tools/yarn.nix
     
     config/pkgs/development/web/nodejs.nix
@@ -27,13 +21,10 @@
 
     config/pkgs/tools/misc/bat.nix
     config/pkgs/tools/misc/exa.nix
-    config/pkgs/tools/misc/neofetch.nix
 
     config/pkgs/tools/nix/nixpkgs-fmt.nix
 
     config/pkgs/tools/package-management/home-manager.nix
     config/pkgs/tools/package-management/nix-prefetch-git.nix
-
-    config/pkgs/tools/system/htop.nix
   ];
 }
